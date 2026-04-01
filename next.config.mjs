243170f31d6f28bfee6237672edd14ef7965b3b4
Al Sukara <alsukara@Als-MacBook-Pro-2.local>
2026-04-01 11:39:18 -0400

    Skip ESLint and TS checks in production build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = { eslint: { ignoreDuringBuilds: true }, typescript: { ignoreBuildErrors: true },
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
